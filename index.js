bef4e383652a68530a42c1d1ad1c5ad6178d3240
rahulrr-coder <rahulrr5002@protonmail.com>
2025-02-08 09:51:44 +0530

    Revert "Reapply "Assignment Complete""
    This reverts commit 5bb84d381eb51fb5a83dc0589446996d40448a00.

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 const express = require('express');
 const { resolve } = require('path');
-const dotenv = require('dotenv');
-dotenv.config();
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.DB_URL)
-    .then(()=>{
-        console.log('MongoDb connected')
-    })
-    .catch((error)=>{
-        console.error('Error connecting to Database')
-    })
 
 const app = express();
 const port = 3010;
